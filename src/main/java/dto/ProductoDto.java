9539a1dd50f8c5fea4de6369dcc47e58528f1b95
mirkoaravena <50345567+mirkoaravena@users.noreply.github.com>
2019-06-03 19:10:34 -0400

    se agrega comentario

--- src/main/java/dto/ProductoDto.java
+++ src/main/java/dto/ProductoDto.java
@@ -9,6 +9,8 @@ package dto;
  *
  * @author PabloTnT
  */
+// Clase java, que contiene tributos, constructor, getters y setters, de los productos
+
 public class ProductoDto {
 
     private int id_producto;
